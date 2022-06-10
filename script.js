7ff2f271c59670030c980a339e29de34756fd9cb
Austin Tirrell <austinjtirrell@gmail.com>
2022-06-09 22:25:34 -0500

    Fixed on-load event

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const header = document.getElementsByTagName('header')[0]
-const body = document.getElementsByTagName('body')[0]
-const mode = document.getElementById('mode')
-const text = document.getElementsByClassName('text')
+const header = document.getElementsByTagName('header')[0];
 
-window.addEventListener('DOMContentLoaded', () => {
-  header.classList.toggle('active')
-})
+function display() {
+  header.classList.toggle('active');
+}
